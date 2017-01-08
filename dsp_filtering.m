504e7b17114e82d10afa6d2b2957439a0406b942
matealex <alexandru_matei2000@yahoo.com>
2017-01-08 22:15:14 +0200

    Remove unused functions

--- dsp_filtering.m
+++ dsp_filtering.m
@@ -257,31 +257,6 @@ if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColo
 end
 
 
-% --- Executes on slider movement.
-function slider_F5_Callback(hObject, eventdata, handles)
-% hObject    handle to slider_F5 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'Value') returns position of slider
-%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
-
-global F5
-F5 = get(handles.slider_F5,'Value');
-set(handles.F5,'String',F5);
-
-% --- Executes during object creation, after setting all properties.
-function slider_F5_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to slider_F5 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: slider controls usually have a light gray background.
-if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor',[.9 .9 .9]);
-end
-
-
 function edit_A1_Callback(hObject, eventdata, handles)
 % hObject    handle to edit_A1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
@@ -410,37 +385,6 @@ function edit_A4_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
-
-
-function edit_A5_Callback(hObject, eventdata, handles)
-% hObject    handle to edit_A5 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of edit_A5 as text
-%        str2double(get(hObject,'String')) returns contents of edit_A5 as a double
-
-global maxA
-global minA
-global A5
-A5 = str2double(get(handles.edit_A5, 'String'));
-
-if (A5 > maxA) || (A5 < minA)
-    msgbox('Please add a value between 0 and 3.');
-end
-
-% --- Executes during object creation, after setting all properties.
-function edit_A5_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to edit_A5 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
-
 
 % --- Executes on button press in btnGenerateS1.
 function btnGenerateS1_Callback(hObject, eventdata, handles)
