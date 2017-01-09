dfb01e0b71f43581ab2c9f540ef453438ee7c969
matealex <alexandru_matei2000@yahoo.com>
2017-01-09 10:36:16 +0200

    Limit graphs in X axis

--- displayGraph.m
+++ displayGraph.m
@@ -2,7 +2,7 @@ function displayGraph(x, y, durata, titleText, limitA, maxA)
 
     plot(x, y, 'g');
     if limitA == 1
-        axis([0, durata, -(maxA+1), (maxA+1)]);
+        axis([0, 1/20*durata, -(maxA+1), (maxA+1)]);
     end            
 
     xlabel('frecventa')
